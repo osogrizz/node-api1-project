de8e54e39df2cdff81f11ea492f8f61fb52689cd
osogrizz <lhtor@gmail.com>
2020-02-25 22:33:49 -0800

    Adds post request.

--- index.js
+++ index.js
@@ -4,22 +4,52 @@ const Users = require('./data/db')
 
 const server = express(); 
 
+server.use(express.json())
+
 server.get('/', (req, res) => {
   res.send('Hello World')
 });
 
+// Gets all users from database.
 server.get('/api/users', (req,res) => {
-  // res.send(Users)
   Users.find()
   .then( users => {
     res.status(200).json(users)
   })
   .catch(err => {
     console.log(err)
-    res.status(500).json({ errorMessage: "Sorry there was an problem finding the users." })
+    res.status(500).json({
+       errorMessage: "Sorry there was an problem finding the users." 
+    })
   })
 });
 
+// Adds a user to the datatbase.
+server.post('/api/users/', (req,res) => {
+  const userData = req.body
+  Users.insert(userData)
+  .then(user => {
+    res.status(201).json(user)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      errorMessage: 'Please provide the name and bio for the user.'
+    })
+  })
+})
 
+server.get('/api/users/:id', (req,res) => {
+  Users.findById(req)
+  .then( user => {
+    res.status(200).json(user)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({
+      errorMessage: 'Sorry I cannot find that user.'
+    })
+  })
+})
 
 server.listen(8000, () => console.log('API running on port 8000'))
